ac7912719d60f177f5a25ce7e1c7bb4fe1ece084
Alan Fernandez <alan.f@msn.com>
2023-08-27 23:03:39 +0200

    sign in page component

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,8 +1,45 @@
-export default function IndexPage() {
+import { type Metadata } from 'next';
+import Link from 'next/link';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/Card';
+import { SignUpForm } from '@/components/forms/SignUpForm';
+import { Shell } from '@/components/shells/Shell';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL),
+  title: 'Sign In',
+  description: 'Sign in to your account',
+};
+
+export default async function SignInPage() {
   return (
-    <main style={{ height: '100vh' }}>
-      <div style={{ width: '100%', padding: '30px 5vw' }}></div>
-    </main>
+    <Shell className='max-w-lg' variant={'centered'}>
+      <Card>
+        <CardHeader className='space-y-1'>
+          <CardTitle className='text-2xl text-center'>Sign up</CardTitle>
+        </CardHeader>
+        <CardContent className='grid gap-4'>
+          <SignUpForm />
+        </CardContent>
+        <CardFooter className='justify-center'>
+          <div className='text-sm text-muted-foreground'>
+            Don`t have an account?{' '}
+            <Link
+              aria-label='Sign Up'
+              href='/signup'
+              className='text-primary underline-offset-4 transition-colors hover:underline'
+            >
+              Sign in
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </Shell>
   );
 }
 
